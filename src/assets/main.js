ea96971bd2af86b51eac038f3cd7a33ab7b58ec0
melenadesign <lexymer@gmail.com>
2017-06-14 02:12:44 +0300

    jquery badges for completd courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,36 @@ $(function() {
 
   // your code will go here
 
+
+  $.ajax({
+    url: 'https://www.codeschool.com/users/923955.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourse(response.courses.completed);// handle response
+    }
+  });
+
+  function addCourse(courses){
+  	var badges = $('#badges');
+
+  	courses.map(function(course){
+  		var div = $('<div class ="course" />').appendTo(badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo(div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo(div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo(div);
+  	}); 
+
+  }
 });
